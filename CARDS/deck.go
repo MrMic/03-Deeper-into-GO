805b40ac54a2fdd246504a0c0e090b697b85317a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-03-18 12:36:50 +0100

    feat(UX): Random Number Generation

--- CARDS/deck.go
+++ CARDS/deck.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -65,8 +66,11 @@ func newDeckFromFile(filename string) deck {
 
 // _____________________________________________________________________
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for idx := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[idx], d[newPosition] = d[newPosition], d[idx]
 	}
 }
