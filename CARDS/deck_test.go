5d47a627c6687336b460cbebf6c39b5e86a04d9f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-03-19 13:05:52 +0100

    feat: create mod.go + Write useful test

--- CARDS/deck_test.go
+++ CARDS/deck_test.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+// TestNewDeck checks that the length of the new deck is 16.
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+}
